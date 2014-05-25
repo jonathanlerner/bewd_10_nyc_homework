96081ab92417047315b2d9d8dd138b5187fe7ca0
jamesbarnes <james.merritt.barnes@gmail.com>
2014-05-25 17:00:27 -0400

    more cleanup

--- _James_Barnes/sent/main.rb
+++ _James_Barnes/sent/main.rb
@@ -4,16 +4,12 @@ require 'json'
 require 'oauth'
 require_relative 'lib/location'
 require_relative 'lib/tweets'
+# require_relative 'lib/score'
 
-
-
-
-
+sents = Hash[File.read('lib/AFINN-111.txt').split(" ").each_slice(2).to_a]
 
 location = Location.new
 location.get_address
 location.confirm_location
 tweets = Tweets.new
 tweets.get_tweets(location)
-puts tweets.parsed_tweets
-
