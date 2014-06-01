39e9b64806be8e05834d4d3091201db50a325ae5
jamesbarnes <james.merritt.barnes@gmail.com>
2014-06-01 08:24:32 -0400

    Further changes

--- _James_Barnes/sent/main.rb
+++ _James_Barnes/sent/main.rb
@@ -13,3 +13,25 @@ location.get_address
 location.confirm_location
 tweets = Tweets.new
 tweets.get_tweets(location)
+
+puts tweets.parsed_tweets
+
+def score_tweets(tweets, sents)
+  score = 0
+  tweets.map do |tweet|
+    tokenized_tweet = tweet.split
+      tokenized_tweet.map do |s|
+        puts
+        if sents[s.downcase] != nil
+          score = score + sents[s.downcase].to_i
+          puts s + " " + sents[s.downcase].to_s + " " + score.to_s
+        end
+      end
+    end
+    puts score
+  end
+
+
+score_tweets(tweets.parsed_tweets, sents)
+
+
