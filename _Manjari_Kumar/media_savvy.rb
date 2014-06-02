bda6998afae6be68e95f00039d73067a2f74d94d
ManjariKumar <manjari.kumar.ny@gmail.com>
2014-06-01 22:48:35 -0400

    midterm submission - user_input, api, conditional processing driven by user input working

--- _Manjari_Kumar/media_savvy.rb
+++ _Manjari_Kumar/media_savvy.rb
@@ -34,29 +34,30 @@ end
 
 def ask_category
   print "\nWhat category of articles are you interested in ? \n Please select one of the options below:\n"
-  print "1. Sports \n2. Politics \n 3. Celebrity \n 4. Food \n 5. Movies\n"
+  print "1. Sports \n2. Politics \n 3. Celebrity \n 4. Food \n 5. Business\n"
   
   user_choice = gets.chomp!
 
-  
-  case user_choice.downcase
-  
+  case user_choice.downcase  
     when "sports", "1"
-      category = 1
+      category = "sports"
+      keywords = "game, tennis, NBA, basketball, US Open, baseball, golf, PGA"
     when "politics", "2"
-      category = 2
+      category = "politics"
+      keywords = "president, prime minister, war, election, state, economy, NSA"
     when "celebrity", "3" 
-      category = 3
+      category = "celebrity"
+      keywords = "Beyonce, justin bieber, oprah, Matt Damon, Jay Carney"
     when "food", "4" 
-      category = 4 
-    when "movies", "5" 
-      category = 5 
+      category = "food"
+      keywords = "sandwich, tacos, pizza, food, organic, vegan"
+    when "business", "5" 
+      category = "business"
+      keywords = "rental, market, CEO, stock, price, brand"
     else print "\nI do not understand that choice\n"
       ask_category  
   end
-
-print "category is : #{category}"       # ask_category
-    # set_category(category)  
+return keywords
 end
 
 
@@ -64,19 +65,24 @@ def show_new_story_notification(story)
   "New story added! Title: #{story[:title]} \nCategory: #{story[:category].capitalize}, \nCurrent Upvotes: #{story[:upvotes]}\n"
 end
 
-def calculate_upvotes(story)
-  story[:upvotes] = 1
-
-  if story[:title].downcase.include? 'horse'
-    story[:upvotes] *= 5
-  elsif story[:title].downcase.include? 'cheddar'
-    story[:upvotes] *=8
-  end
-
-  if story[:category].downcase == "food"
-    story[:upvotes] *= 3
-  end
+def category_filter(stories, keywords)
+  counter=0
+  keyword_array = keywords.split(%r{,\s*})
+  print keyword_array
+  stories.each do |s|
+    keyword_array.any? do |word|
+      if s[:body_text].downcase.include? word.downcase
+      counter+=1
+      print "\n#{counter}. #{s[:title]} \nwas shared #{s[:facebook]} times on facebook\n"
+      print "\nwas shared #{s[:twitter]} times on twitter\n"
+      print "\nwas shared #{s[:google_plus]} times on google_plus\n"
+      print "\nwas shared #{s[:linked_in]} times on linked_in\n"
+      print "\nwas shared #{s[:total]} times in total\n" 
+      end
+    end
+  end 
 end
+
 
 def show_all_stories(stories)
   #Complete this section with both map and each. Highlight the difference of each.
@@ -88,25 +94,20 @@ end
 
   def get_from_mashable
   res = JSON.load(RestClient.get('http://mashable.com/stories.json'))
-  # puts "class of res is : #{res['new'][2]["shares"].keys}"
-
+  all_stories = []
   res["new"].each do |story|
-    s = {title: story["title"], story: story["content"]["plain"], facebook: story["shares"]["facebook"], twitter: story["shares"]["twitter"],
+    s = {title: story["title"], body_text: story["content"]["plain"], facebook: story["shares"]["facebook"], twitter: story["shares"]["twitter"],
       google_plus: story["shares"]["google_plus"], linked_in: story["shares"]["linked_in"], total: story["shares"]["total"]}
-    print "\n#{s[:title]} \nwas shared #{s[:facebook]} times on facebook\n"
-    print "\nwas shared #{s[:twitter]} times on twitter\n"
-    print "\nwas shared #{s[:google_plus]} times on google_plus\n"
-    print "\nwas shared #{s[:linked_in]} times on linked_in\n"
-    print "\nwas shared #{s[:total]} times in total\n"
+    all_stories << s
     end
+    return all_stories
   end
-
 
 
 stories = get_from_mashable
 puts "Welcome to Media Savvy! A media selecion adviser. It's all about location, location, location!"
-ask_category
-
+keywords = ask_category
+category_filter(stories, keywords)    
 # show_all_stories stories
 
 
