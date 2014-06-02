401ddba4cdac4bb52827d1fb37e1b399d1f5c3ce
ManjariKumar <manjari.kumar.ny@gmail.com>
2014-06-02 16:30:21 -0400

    trying to get the entire recommendation logic out of main.rb into its own class

--- _Manjari_Kumar/midterm_submission/lib/keyword.rb
+++ _Manjari_Kumar/midterm_submission/lib/keyword.rb
@@ -7,10 +7,6 @@ class Keyword
   end
 
   def split_into_array
-  	if keyword_string != nil
   	  @keyword_array = keyword_string.split(%r{,\s*})
-  	else
-  	  @keyword_array = []
-  	end
   end 
 end
